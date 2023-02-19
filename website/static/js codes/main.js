9a94598fbb529d6967580a360871b51c2b27bcfc
wathika <71040609+wathika-eng@users.noreply.github.com>
2023-02-19 12:51:45 +0300

    Update main.js

--- website/static/js codes/main.js
+++ website/static/js codes/main.js
@@ -1,8 +1,1 @@
-import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
-const appInsights = new ApplicationInsights({ config: {
-  connectionString: 'Copy connection string from Application Insights Resource Overview'
-  /* ...Other Configuration Options... *///
-} });
-appInsights.loadAppInsights();
-appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
