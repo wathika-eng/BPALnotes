a254dcd8372f362278529b0a57f4a2eedc2dd955
wathika <71040609+wathika-eng@users.noreply.github.com>
2023-02-19 18:19:08 +0300

    Add files via upload

--- website/static/js codes/main.js
+++ website/static/js codes/main.js
@@ -1,1 +1,8 @@
+//import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
+//const appInsights = new ApplicationInsights({ config: {
+  connectionString: ''
+  /* ...Other Configuration Options... *///
+//} });
+//appInsights.loadAppInsights();
+//appInsights.trackPageView(); // Manually call trackPageView to establish the current user/session/pageview
--- website/views.py
+++ website/views.py
@@ -13,24 +13,54 @@ def home():
 @views.route("/error")
 def error():
     return render_template('404.html')
-
+##################YEARS OF STUDY#######################################################
 @views.route("/BPAL 1st year")
 def first():
-    return render_template('1st.html')
+    return render_template('1select.html')
 
 @views.route("/BPAL 2nd year")
 def second():
-    return render_template('2ndhome.html')
+    return render_template('select.html')
 
 @views.route("/BPAL 3rd year")
 def third():
-    return render_template('3rdhome.html')
+    return render_template('3select.html')
 
 #4thyr
 @views.route("/research")
 def resea():
     return render_template('research.html')
 
+##########################SEMESTER#################################
+@views.route("/1.1")
+def one():
+    return render_template('1sthome.html')
+
+@views.route("/1.2")
+def oone():
+    return render_template('11sthome.html')
+
+@views.route("/2.1")
+def sec():
+    return render_template('2ndhome.html')
+
+@views.route("/2.2")
+def secc():
+    return render_template('22ndhome.html')
+
+@views.route("/3.1")
+def three():
+    return render_template('3rdhome.html')
+
+@views.route("/3.2")
+def threee():
+    return render_template('33rdhome.html')
+
+
+#########################################################################
+@views.route("/project")
+def proj():
+    return render_template('project.html')
 
 @views.route("/pfm")
 def pfm():
