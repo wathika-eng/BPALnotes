417aea8901d298bf2f3bd504213f9c11cc8931ef
wathika <71040609+wathika-eng@users.noreply.github.com>
2023-02-22 23:09:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ from flask import Flask
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
     app.run(host = '0.0.0.0', port =5000)
  
     
